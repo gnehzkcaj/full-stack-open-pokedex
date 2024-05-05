c58fc2f019aa1deb8b60aa481af284fec81dc8eb
gnehzkcaj <97222543+gnehzkcaj@users.noreply.github.com>
2024-05-05 01:32:48 +0000

    sample test update

--- playwright/tests/example.spec.js
+++ playwright/tests/example.spec.js
@@ -1,19 +1,27 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')
+require('dotenv').config()
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/')
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await expect(page).toHaveTitle(/Playwright/)
+})
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/')
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Get started' }).click()
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible()
+})
+
+test('front page can be opened', async ({ page }) => {
+  const baseUrl = process.env.BASE_URL !== undefined ? process.env.BASE_URL : 'https://default-url.com'
+  await page.goto(baseUrl)
+  //await expect(page.getByText('ivysaur')).toBeVisible()
+  await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+})
